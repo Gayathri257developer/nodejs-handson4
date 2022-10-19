30b8e29882ce4a3faab2e4994b4d3eab2f2038e2
gayathri <gayathri.ramakrishna158@gmail.com>
2022-10-19 18:48:24 +0530

    handson 4

---
+++ db.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+      email: "dr@gmail.com",
+      password: "123456"
+  }
+]
+
+const publicPosts = [
+  {
+      title: "Free Tips on Development",
+      content: "These are some tips"
+  }
+]
+
+const privatePosts = [
+  {
+      title: "Paid Tips on Development",
+      content: "These are some tips"
+  }
+]
+
+module.exports = {
+  users,
+  publicPosts,
+  privatePosts
+}
